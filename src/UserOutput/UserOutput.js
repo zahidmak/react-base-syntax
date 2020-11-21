721337d2f83a54e2afe8ab1e38b6823a91592560
Zahid Maknojia <zahid_mak@outlook.com>
2020-11-20 20:07:20 -0500

    All files

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const useroutput = (props) => {
+
+    return(
+        <div>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+        </div>
+        
+    );
+}
+
+export default useroutput;
